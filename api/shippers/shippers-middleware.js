6c376122a763aaf616f57629e9c84759b1c268cf
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-05 10:48:12 -0500

    done

--- api/shippers/shippers-middleware.js
+++ api/shippers/shippers-middleware.js
@@ -1,14 +1,32 @@
+const db = require('../../data/db-config')
 
+
+
+async function checkId(req, res, next) {
+  const shipper = await db('shippers')
+    .where('shipperid', req.params.id).first()
+  if (!shipper) {
+    next({
+      status: 404,
+      message: `no shipper with id:${req.params.id}`
+    })
+  }
+  else next()
+}
+
+function checkPayload(req, res, next) {
+  if (!req.body.Phone || !req.body.ShipperName) {
+    next({
+      status: 422,
+      message: 'phone and ShipperName are required'
+    })
+  }
+  else {
+    next();
+  }
+}
 
 module.exports = {
   checkId,
   checkPayload,
-}
-
-function checkId(req, res, next) {
-  next()
-}
-
-function checkPayload(req, res, next) {
-  next()
 }
